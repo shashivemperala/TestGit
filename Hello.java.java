3e85f398dd459fa8a5bb533afeb63ad50f022ee8
shashivemperala <shashivemperala21@gmail.com>
2019-07-16 13:02:00 +0530

    Test Contains Hello.java,Hello.class & README.md

---
+++ Hello.java.java
@@ -0,0 +1,14 @@
+class Hello {
+
+public static void main(String[] args){
+
+
+System.out.println("hello welcome to Git World  ");
+
+
+
+
+
+}
+
+}
---
+++ README.md.md
@@ -0,0 +1,3 @@
+//README.md
+This is a readme file for project called Test that 
+has 1 class and 1 java file.
